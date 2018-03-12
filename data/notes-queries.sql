3bd0a80dc4c113b8ab433f02819f700212384e0e
Anya <anya.p.nguyen@gmail.com>
2018-03-12 14:05:15 -0700

    added sql database and queries

---
+++ data/notes-queries.sql
@@ -0,0 +1,24 @@
+-- SELECT * FROM notes;
+
+-- SELECT * FROM notes
+-- LIMIT 5;
+
+-- SELECT id FROM notes
+-- ORDER BY id DESC;
+
+-- SELECT * FROM notes
+-- WHERE title = 'What the government doesn''t want you to know about cats';
+
+-- SELECT * FROM notes
+-- WHERE title LIKE '7%';
+
+-- UPDATE notes SET
+--     title = 'Anya''s updates',
+--     content = 'Random Stuff'
+--     WHERE id = 1009;
+
+-- INSERT INTO notes ( id, title, content ) VALUES
+--     (1011, 'Anya''s second update', 'Second random stuff');
+
+-- DELETE FROM notes 
+-- WHERE id = 1006;
